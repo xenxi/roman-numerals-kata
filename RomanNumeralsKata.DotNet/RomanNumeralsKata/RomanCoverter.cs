c74f83a1d351e5ed292c6db120bec85258430887
andiaz <andiaz@aidacanarias.com>
2022-05-31 20:41:13 +0200

    :recycle: refact introduce constant

--- RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
+++ RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
@@ -1,14 +1,21 @@
-﻿namespace RomanNumeralsKata
+﻿using System.Collections.Generic;
+
+namespace RomanNumeralsKata
 {
     public class RomanCoverter
     {
+        private static readonly Dictionary<int, string> arabicToRomanSymbols = new() {
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"},
+        };
         public string Convert(int number) {
             if (number >= 5)
-                return "V" + Convert(number - 5);
+                return arabicToRomanSymbols[5] + Convert(number - 5);
             if (number >= 4)
-                return "IV" + Convert(number - 4);
+                return arabicToRomanSymbols[4] + Convert(number - 4);
             if (number >= 1)
-                return "I" + Convert(number - 1);
+                return arabicToRomanSymbols[1] + Convert(number - 1);
 
             return string.Empty;
         }
