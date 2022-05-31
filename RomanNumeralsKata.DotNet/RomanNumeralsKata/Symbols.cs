3c9c615d104b069bdc084b99013301a414870bc6
andiaz <andiaz@aidacanarias.com>
2022-05-31 21:37:52 +0200

    :recycle: refact inline variable

--- RomanNumeralsKata.DotNet/RomanNumeralsKata/Symbols.cs
+++ RomanNumeralsKata.DotNet/RomanNumeralsKata/Symbols.cs
@@ -1,9 +1,24 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace RomanNumeralsKata;
 
 public class Symbols {
     public Symbol FindClosestSymbol(int number) {
-        return RomanCoverter.arabicToRomanSymbols.First(x => x.Arabic <= number);
+        var arabicToRomanSymbols = new List<Symbol>() {
+            new Symbol(900, "CM"),
+            new Symbol(500, "D"),
+            new Symbol( 400, "CD"),
+            new Symbol( 100, "C"),
+            new Symbol( 90, "XC"),
+            new Symbol( 50, "L"),
+            new Symbol( 40, "XL"),
+            new Symbol( 10, "X"),
+            new Symbol( 9, "IX"),
+            new Symbol(5, "V"),
+            new Symbol(4, "IV"),
+            new Symbol(1, "I"),
+        };
+        return arabicToRomanSymbols.First(x => x.Arabic <= number);
     }
 }
