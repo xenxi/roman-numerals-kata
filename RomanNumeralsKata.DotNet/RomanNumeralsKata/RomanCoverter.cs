155bec3a35f13aad1e3b3a94024d9aeb698d10b5
andiaz <andiaz@aidacanarias.com>
2022-05-31 21:33:41 +0200

    :recycle: refact use class

--- RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
+++ RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
@@ -5,19 +5,19 @@ namespace RomanNumeralsKata
 {
     public class RomanCoverter
     {
-        private static readonly Dictionary<int, string> arabicToRomanSymbols = new() {
-            {900, "CM"},
-            {500, "D"},
-            { 400, "CD"},
-            { 100, "C"},
-            { 90, "XC"},
-            { 50, "L"},
-            { 40, "XL"},
-            { 10, "X"},
-            { 9, "IX"},
-            {5, "V"},
-            {4, "IV"},
-            {1, "I"},
+        private static readonly List<Symbol> arabicToRomanSymbols = new() {
+            new Symbol(900, "CM"),
+            new Symbol(500, "D"),
+            new Symbol( 400, "CD"),
+            new Symbol( 100, "C"),
+            new Symbol( 90, "XC"),
+            new Symbol( 50, "L"),
+            new Symbol( 40, "XL"),
+            new Symbol( 10, "X"),
+            new Symbol( 9, "IX"),
+            new Symbol(5, "V"),
+            new Symbol(4, "IV"),
+            new Symbol(1, "I"),
         };
         public string Convert(int number) {
             if (number < 1) return string.Empty;
@@ -27,8 +27,7 @@ namespace RomanNumeralsKata
         }
 
         private static Symbol FindClosestSymbol(int number) {
-            var (symbolValue, symbol) = arabicToRomanSymbols.First(x => x.Key <= number);
-            return new Symbol(symbolValue, symbol);
+            return arabicToRomanSymbols.First(x => x.Arabic <= number);
         }
     }
 }
