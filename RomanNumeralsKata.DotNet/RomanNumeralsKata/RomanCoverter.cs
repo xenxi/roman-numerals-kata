4ec52c09cf502968e239393fad41b2fbf59ec5da
andiaz <andiaz@aidacanarias.com>
2022-05-31 09:18:12 +0200

    :recycle: refact reduce cyclomatic complexity

--- RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
+++ RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
@@ -1,12 +1,12 @@
-﻿namespace RomanNumeralsKata {
-    public class RomanCoverter {
-        public string Convert(int number) {
-            if (number == 1)
-                return "I" + Convert(0);
-            if (number == 2)
-                return "I" + Convert(1);
-            if (number == 3)
-                return "I" + Convert(2);
+﻿namespace RomanNumeralsKata
+{
+    public class RomanCoverter
+    {
+        public string Convert(int number)
+        {
+            if (number > 0)
+                return "I" + Convert(number - 1);
+
             return string.Empty;
         }
     }
