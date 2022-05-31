0e1f3fd7f1483981d54fc88f02cf34e3bc9cf4da
andiaz <andiaz@aidacanarias.com>
2022-05-31 21:39:51 +0200

    :recycle: refact introduce field

--- RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
+++ RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
@@ -1,28 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace RomanNumeralsKata
+﻿namespace RomanNumeralsKata
 {
     public class RomanCoverter
     {
-        public static readonly List<Symbol> arabicToRomanSymbols = new() {
-            new Symbol(900, "CM"),
-            new Symbol(500, "D"),
-            new Symbol( 400, "CD"),
-            new Symbol( 100, "C"),
-            new Symbol( 90, "XC"),
-            new Symbol( 50, "L"),
-            new Symbol( 40, "XL"),
-            new Symbol( 10, "X"),
-            new Symbol( 9, "IX"),
-            new Symbol(5, "V"),
-            new Symbol(4, "IV"),
-            new Symbol(1, "I"),
-        };
+        private readonly Symbols _symbols = new();
+
         public string Convert(int number) {
             if (number < 1) return string.Empty;
 
-            var symbol = new Symbols().FindClosestSymbol(number);
+            var symbol = _symbols.FindClosestSymbol(number);
             return symbol.Roman + Convert(number - symbol.Arabic);
         }
     }
