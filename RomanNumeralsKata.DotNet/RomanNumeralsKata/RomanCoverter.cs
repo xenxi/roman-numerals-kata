483e23e542e63e9f295a84c2ffa3d5a865697d9e
andiaz <andiaz@aidacanarias.com>
2022-05-31 20:30:41 +0200

    :recycle:

--- RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
+++ RomanNumeralsKata.DotNet/RomanNumeralsKata/RomanCoverter.cs
@@ -3,11 +3,11 @@
     public class RomanCoverter
     {
         public string Convert(int number) {
-            if (number == 5)
+            if (number >= 5)
                 return "V" + Convert(number - 5);
-            if (number == 4)
+            if (number >= 4)
                 return "IV" + Convert(number - 4);
-            if (number > 0)
+            if (number >= 1)
                 return "I" + Convert(number - 1);
 
             return string.Empty;
