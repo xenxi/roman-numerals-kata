e604f3478d52e8ce4a2d5bd681d43018cbc94ebd
andiaz <andiaz@aidacanarias.com>
2022-05-31 21:11:27 +0200

    :fire: delete duplicated tests

--- RomanNumeralsKata.DotNet/RomanNumeralsKata.Tests/RomanConverterShould.cs
+++ RomanNumeralsKata.DotNet/RomanNumeralsKata.Tests/RomanConverterShould.cs
@@ -5,63 +5,6 @@ namespace RomanNumeralsKata.Tests
 {
     public class RomanConverterShould
     {
-
-        [Test]
-        public void convert_zero_to_a_empty_string() {
-            var converter = new RomanCoverter();
-
-            var romanNumeral = converter.Convert(0);
-
-            romanNumeral.Should().Be(string.Empty);
-        }
-
-        [Test]
-        public void convert_one_to_I()
-        {
-            var converter = new RomanCoverter();
-
-            var romanNumeral = converter.Convert(1);
-
-            romanNumeral.Should().Be("I");
-        }
-
-        [Test]
-        public void convert_two_to_II()
-        {
-            var converter = new RomanCoverter();
-
-            var romanNumeral = converter.Convert(2);
-
-            romanNumeral.Should().Be("II");
-        }
-        [Test]
-        public void convert_three_to_III()
-        {
-            var converter = new RomanCoverter();
-
-            var romanNumeral = converter.Convert(3);
-
-            romanNumeral.Should().Be("III");
-        }
-        [Test]
-        public void convert_four_to_IV()
-        {
-            var converter = new RomanCoverter();
-
-            var romanNumeral = converter.Convert(4);
-
-            romanNumeral.Should().Be("IV");
-        }
-
-        [Test]
-        public void convert_five_to_V()
-        {
-            var converter = new RomanCoverter();
-
-            var romanNumeral = converter.Convert(5);
-
-            romanNumeral.Should().Be("V");
-        }
         [TestCase(1, "I")]
         [TestCase(2, "II")]
         [TestCase(3, "III")]
